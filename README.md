855c7c3754bc5758e8bd5e367972162f9e0648a2
Jack Watters <johnwatt@usc.edu>
2022-08-16 00:40:50 -0700

    Styling first draft + insignificant css tweaks.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # odp-tic-tac-toe
 
+Eventually add ability to edit player names.
+
 Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
     1. Start by just getting the computer to make a random legal move.
     2. Once you’ve gotten that, work on making the computer smart. It is possible to create an unbeatable AI using the minimax algorithm (read about it here, some googling will help you out with this one)
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // 1. store gameboard as an array inside a gameboard object
+
 // 2. players will be stored in onjects
 //     a. create with factories
+
 // 3. object to control the flow of the gameboard
+
 // 4. use modules if you ever need one of something
 //     a. gameboard
 //     b. displayControlled
+
 // 5. use as little global code possible - stor in module or factor when possible
+
 // 6. JavaScript function that will render the contents of the gameboard array to the webpage 
 //     (for now you can just manually fill in the array with "X"s and "O"s)
+
 // 7. Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
