0adda8cbf37dd7237deb9f314ebd9b8bcdb91a0b
Jack Watters <johnwatt@usc.edu>
2022-08-17 00:47:16 -0700

    begun setting up js

--- script.js
+++ script.js
@@ -1,17 +1,91 @@
-// 1. store gameboard as an array inside a gameboard object
+// use as little global code possible - store in module or factor when possible
 
-// 2. players will be stored in onjects
-//     a. create with factories
+// Gameboard object - Module
+// Gonna have to change to 2 grid array for maths
+const gameBoard = (() => {
+  // store gameboard as an array inside a gameboard object
+  //   let gameArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8],];
+  let gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-// 3. object to control the flow of the gameboard
+  const getBoard = () => {
+    console.log(gameArray);
+    return gameArray;
+  };
 
-// 4. use modules if you ever need one of something
-//     a. gameboard
-//     b. displayControlled
+  const clearBoard = () => {
+    gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  };
 
-// 5. use as little global code possible - stor in module or factor when possible
+  const setValue = (a, val) => {
+    gameArray[a] = val;
+  };
 
-// 6. JavaScript function that will render the contents of the gameboard array to the webpage 
-//     (for now you can just manually fill in the array with "X"s and "O"s)
+  return { getBoard, clearBoard, setValue };
+})();
 
-// 7. Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
+//
+//
+// Dumb Scratch
+// object to control the flow of the game - Module
+let restartButton = document.querySelector("button.restart");
+let playerArea = document.querySelector(".players");
+let boxes = document.querySelectorAll(".box");
+let players = document.querySelectorAll(".player");
+
+//   const displayController = () => {
+let clearScreen = restartButton.addEventListener("click", () => {
+  boxes.forEach((box) => {
+    box.innerHTML = "";
+  });
+  gameBoard.clearBoard();
+  gameBoard.getBoard();
+});
+
+let turn = "X";
+let addToBox = boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (!box.innerHTML) {
+      box.innerHTML = turn;
+      if (turn == "X") {
+        turn = "O";
+      } else {
+        turn = "X";
+      }
+
+      players.forEach((player) => {
+        player.classList.toggle("glow");
+      });
+
+      gameBoard.setValue(box.id, box.innerHTML);
+    }
+    gameBoard.getBoard();
+  });
+});
+//   };
+
+//
+//
+//
+// 2. player objects - factories
+const player = (name, sign) => {
+  let _name = name;
+  let _sign = sign;
+
+  //   const setSign = () => {
+  //     let boxes = document.querySelectorAll(".box");
+  //     boxes.forEach((box) => {
+  //       box.addEventListener("click", function selectBox() {
+  //         alert("yowza");
+  //       });
+  //     });
+  //   };
+  return { _name, _sign };
+};
+
+//
+//
+//
+
+// Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
+const gameOver = function () {};
+// })();
