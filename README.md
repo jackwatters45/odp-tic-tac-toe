e3cad35559b413541925c1ca95af783c2185f31f
Jack Watters <johnwatt@usc.edu>
2022-08-27 23:46:30 -0700

    logic for checking for winner and began working on overlay.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # odp-tic-tac-toe
 
-1. Think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects.. but take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-2. Figure out hoe to make 2d array or do the math Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
-3. Clean up the interface to allow players to put in their names, include a button to start/restart the game and add a display element that congratulates the winning player!
+turn all this bs into functions and shit
+
+organize logic
+
+make sure to actually use objects
+
+add winner display
 
 
-
-Eventually add ability to edit player names.
-Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
+Eventually 
+1.  add ability to edit player names.
+2.  create an AI so that a player can play against the computer!
     1. Start by just getting the computer to make a random legal move.
     2. Once you’ve gotten that, work on making the computer smart. It is possible to create an unbeatable AI using the minimax algorithm (read about it here, some googling will help you out with this one)
     3. If you get this running definitely come show it off in the chatroom. It’s quite an accomplishment!
--- script.js
+++ script.js
@@ -1,91 +1,154 @@
-// use as little global code possible - store in module or factor when possible
-
 // Gameboard object - Module
-// Gonna have to change to 2 grid array for maths
 const gameBoard = (() => {
   // store gameboard as an array inside a gameboard object
-  //   let gameArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8],];
   let gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
   const getBoard = () => {
-    console.log(gameArray);
     return gameArray;
   };
-
   const clearBoard = () => {
     gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   };
-
   const setValue = (a, val) => {
     gameArray[a] = val;
   };
-
   return { getBoard, clearBoard, setValue };
 })();
 
-//
-//
-// Dumb Scratch
 // object to control the flow of the game - Module
-let restartButton = document.querySelector("button.restart");
-let playerArea = document.querySelector(".players");
-let boxes = document.querySelectorAll(".box");
-let players = document.querySelectorAll(".player");
+const displayController = (() => {
+  let restartButton = document.querySelector("button.restart");
+  let boxes = document.querySelectorAll(".box");
+  let players = document.querySelectorAll(".player");
+  let turn = "X"; // X always starts - too bad
 
-//   const displayController = () => {
-let clearScreen = restartButton.addEventListener("click", () => {
-  boxes.forEach((box) => {
-    box.innerHTML = "";
+  // clears game when user clicks restart button --> probably gonna have to add a thing for resetting after winner
+  // turn into function
+  let clearScreen = restartButton.addEventListener("click", () => {
+    boxes.forEach((box) => {
+      box.innerHTML = "";
+    });
+    gameBoard.clearBoard();
   });
-  gameBoard.clearBoard();
-  gameBoard.getBoard();
-});
 
-let turn = "X";
-let addToBox = boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    if (!box.innerHTML) {
-      box.innerHTML = turn;
-      if (turn == "X") {
-        turn = "O";
-      } else {
-        turn = "X";
-      }
-
-      players.forEach((player) => {
-        player.classList.toggle("glow");
+  // make this pretty much just functions
+  let playGame = () => {
+    // make two players
+    // make board
+    boxes.forEach((box) => {
+      box.addEventListener("click", () => {
+        if (!box.innerHTML) {
+          box.innerHTML = turn;
+          if (turn == "X") {
+            turn = "O";
+          } else {
+            turn = "X";
+          }
+          players.forEach((player) => {
+            player.classList.toggle("glow");
+          });
+          gameBoard.setValue(box.id, box.innerHTML);
+        }
+        checkForWinner();
       });
+    });
+  };
 
-      gameBoard.setValue(box.id, box.innerHTML);
+  // when winner found, display winner pop up
+  let displayWinner = (winner) => {
+    alert("Player " + winner + " wins!");
+  };
+
+  // Check for winner --> clean up with functions etc. later
+  let checkForWinner = () => {
+    console.log(gameBoard.getBoard());
+    for (let j = 0; j < 3; j++) {
+      let x = 0;
+      let o = 0;
+      for (let i = 0 + j; i <= 6 + j; i += 3) {
+        if (gameBoard.getBoard()[i] == "X") {
+          x++;
+        } else if (gameBoard.getBoard()[i] == "O") {
+          o++;
+        }
+      }
+      if (x == 3) {
+        displayWinner("X");
+      }
+      if (o == 3) {
+        displayWinner("O");
+      }
     }
-    gameBoard.getBoard();
-  });
-});
-//   };
+    for (let j = 0; j <= 6; j += 3) {
+      let x = 0;
+      let o = 0;
+      for (let i = 0 + j; i < 3 + j; i++) {
+        if (gameBoard.getBoard()[i] == "X") {
+          x++;
+        } else if (gameBoard.getBoard()[i] == "O") {
+          o++;
+        }
+      }
+      if (x == 3) {
+        console.log("x is winner");
+      }
+      if (o == 3) {
+        console.log("o is winner");
+      }
+    }
 
-//
-//
-//
+    let firstCross = () => {
+      let x = 0;
+      let o = 0;
+      for (let i = 0; i <= 8; i += 4) {
+        if (gameBoard.getBoard()[i] == "X") {
+          x++;
+        } else if (gameBoard.getBoard()[i] == "O") {
+          o++;
+        }
+      }
+      if (x == 3) {
+        console.log("x is winner");
+      }
+      if (o == 3) {
+        console.log("o is winner");
+      }
+    };
+
+    firstCross();
+
+    let secondCross = () => {
+      let x = 0;
+      let o = 0;
+      for (let i = 2; i <= 6; i += 2) {
+        if (gameBoard.getBoard()[i] == "X") {
+          x++;
+        } else if (gameBoard.getBoard()[i] == "O") {
+          o++;
+        }
+      }
+      if (x == 3) {
+        console.log("x is winner");
+      }
+      if (o == 3) {
+        console.log("o is winner");
+      }
+    };
+
+    secondCross();
+
+    // TODO: check for tie 
+
+  };
+
+  return { playGame, clearScreen };
+})();
+
 // 2. player objects - factories
 const player = (name, sign) => {
   let _name = name;
   let _sign = sign;
 
-  //   const setSign = () => {
-  //     let boxes = document.querySelectorAll(".box");
-  //     boxes.forEach((box) => {
-  //       box.addEventListener("click", function selectBox() {
-  //         alert("yowza");
-  //       });
-  //     });
-  //   };
   return { _name, _sign };
 };
 
-//
-//
-//
-
-// Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
-const gameOver = function () {};
-// })();
+displayController.playGame();
