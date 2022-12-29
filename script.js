72a44cd42ec339d2e435758843b77ead0ffc3c5b
Jack Watters <johnwatt@usc.edu>
2022-12-28 22:01:55 -0800

    improved methods and cleaned up syntax

--- script.js
+++ script.js
@@ -8,109 +8,85 @@ const gameBoard = (() => {
   const clearBoard = () => {
     gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   };
-  const setValue = (a, val) => {
-    gameArray[a] = val;
+  const setValue = (cell, val) => {
+    gameArray[cell] = val;
   };
   return { getBoard, clearBoard, setValue };
 })();
 
 // object to control the flow of the game - Module
 const displayController = (() => {
-  let restartButton = document.querySelector("button.restart");
-  let boxes = document.querySelectorAll(".box");
-  let players = document.querySelectorAll(".player");
-  let overlay = document.querySelector(".overlay");
-  let turn = "X"; // X always starts - too bad
+  const restartButton = document.querySelector('button.restart');
+  const boxes = document.querySelectorAll('.box');
+  const players = document.querySelectorAll('.player');
+  const overlay = document.querySelector('.overlay');
+  let turn = 'X';
 
   // clears game when user clicks restart button
-  let clearScreen = () => {
-    restartButton.addEventListener("click", () => {
-      boxes.forEach((box) => {
-        box.innerHTML = "";
-      });
-      overlay.classList.add("hidden");
+  const clearScreen = () => {
+    restartButton.addEventListener('click', () => {
+      boxes.forEach((box) => (box.innerHTML = ''));
+      overlay.classList.add('hidden');
       gameBoard.clearBoard();
     });
   };
 
   // when winner found, display winner overlay
-  let displayWinner = (winner) => {
+  const displayWinner = (winner) => {
     let winnerText = overlay.firstElementChild;
     winnerText.textContent = `Player ${winner} wins!`;
-    overlay.classList.remove("hidden");
+    overlay.classList.remove('hidden');
   };
 
   // when tie found, display tie overlay
-  let displayTie = () => {
-    let winnerText = overlay.firstElementChild;
+  const displayTie = () => {
+    const winnerText = overlay.firstElementChild;
     winnerText.textContent = `It's a tie!`;
-    overlay.classList.remove("hidden");
+    overlay.classList.remove('hidden');
   };
 
   // Check for winner -> four different ways to win and a check for tie
-  let checkForWinner = (turn) => {
-    let vertical = () => {
-      for (let j = 0; j < 3; j++) {
+  const checkForWinner = (turn) => {
+    const currentBoard = gameBoard.getBoard();
+    const vertical = () => {
+      for (let j = 0; j < 3; j += 1) {
         let count = 0;
         for (let i = 0 + j; i <= 6 + j; i += 3) {
-          if (gameBoard.getBoard()[i] == turn) {
-            count++;
-          }
+          if (currentBoard[i] === turn) count++;
         }
-        if (count == 3) {
-          displayWinner(turn);
-        }
+        if (count === 3) displayWinner(turn);
       }
     };
 
-    let horizontal = () => {
+    const horizontal = () => {
       for (let j = 0; j <= 6; j += 3) {
         let count = 0;
-        for (let i = 0 + j; i < 3 + j; i++) {
-          if (gameBoard.getBoard()[i] == turn) {
-            count++;
-          }
+        for (let i = 0 + j; i < 3 + j; i += 1) {
+          if (currentBoard[i] === turn) count++;
         }
-        if (count == 3) {
-          displayWinner(turn);
-        }
+        if (count === 3) displayWinner(turn);
       }
     };
 
-    let firstCross = () => {
+    const firstCross = () => {
       let count = 0;
       for (let i = 0; i <= 8; i += 4) {
-        if (gameBoard.getBoard()[i] == turn) {
-          count++;
-        }
+        if (currentBoard[i] === turn) count++;
       }
-      if (count == 3) {
-        displayWinner(turn);
-      }
+      if (count === 3) displayWinner(turn);
     };
 
-    let secondCross = () => {
+    const secondCross = () => {
       let count = 0;
       for (let i = 2; i <= 6; i += 2) {
-        if (gameBoard.getBoard()[i] == turn) {
-          count++;
-        }
+        if (currentBoard[i] === turn) count++;
       }
-      if (count == 3) {
-        displayWinner(turn);
-      }
+      if (count === 3) displayWinner(turn);
     };
 
-    let tie = () => {
-      let totalCount = 0;
-      gameBoard.getBoard().forEach((box) => {
-        if (typeof box === "string") {
-          totalCount++;
-        }
-      });
-      if (totalCount == 9) {
+    const tie = () => {
+      if (currentBoard.filter((box) => typeof box === 'string').length === 9)
         displayTie();
-      }
     };
 
     vertical();
@@ -121,42 +97,26 @@ const displayController = (() => {
   };
 
   // Toggles turn dispkay and changes current turn
-  let togglePlayerTurn = () => {
-    players.forEach((player) => {
-      player.classList.toggle("glow");
-    });
-    if (turn == "X") {
-      turn = "O";
-    } else {
-      turn = "X";
-    }
+  const togglePlayerTurn = () => {
+    players.forEach((player) => player.classList.toggle('glow'));
+    turn = turn === 'X' ? 'O' : 'X';
   };
 
   // Actually play the game
-  let playGame = () => {
+  const playGame = () => {
     clearScreen();
     boxes.forEach((box) => {
-      box.addEventListener("click", () => {
-        if (!box.innerHTML) {
-          box.innerHTML = turn;
-          gameBoard.setValue(box.id, box.innerHTML);
-          checkForWinner(turn);
-          togglePlayerTurn();
-        }
+      box.addEventListener('click', () => {
+        if (box.innerHTML) return;
+        box.innerHTML = turn;
+        gameBoard.setValue(box.id, turn);
+        checkForWinner(turn);
+        togglePlayerTurn();
       });
     });
   };
 
   return { playGame };
 })();
-
-// player objects - factories
-// TODO: develop when I add the ability to add user name
-const player = (name, sign) => {
-  let getName = () => name;
-  let getSign = () => sign;
-
-  return { getName, getSign };
-};
 
 displayController.playGame();
